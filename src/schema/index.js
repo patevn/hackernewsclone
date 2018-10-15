86f3dbfb96562ffe080d3d726a2f76a9ea1d5807
Peter Mc <petermmcnaught@gmail.com>
2018-10-15 20:06:46 +1100

    Init commit, a basic working graphql server with graphiql

---
+++ src/schema/index.js
@@ -0,0 +1,18 @@
+const { makeExecutableSchema } = require('graphql-tools');
+const resolvers = require('./resolvers');
+
+// Define your types here.
+const typeDefs = 
+` type Link { 
+    id: ID!
+    url: String! 
+    description: String!
+ } 
+ 
+ type Query {
+      allLinks: [Link!]! 
+    }
+  `;
+
+// Generate the schema object from your types definition.
+module.exports = makeExecutableSchema({ typeDefs, resolvers });
