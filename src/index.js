9f37f84f13e04cec472c9065df788cb58256ff32
Peter Mc <petermmcnaught@gmail.com>
2018-10-18 20:34:08 +1100

    Added mongo and connectors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 const express = require('express');
-
-// This package automatically parses JSON requests.
 const bodyParser = require('body-parser');
-
-// This package will handle GraphQL server requests and responses
-// for you, based on your schema.
-
-
-const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
-
+const {
+  graphqlExpress,
+  graphiqlExpress
+} = require('apollo-server-express');
 const schema = require('./schema');
 
-var app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+// 1
+const connectMongo = require('./mongo-connector');
 
-app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-}));
+// 2
+const start = async () => {
+  // 3
+  try {
+    const mongo = await connectMongo();
+    var app = express();
+    app.use('/graphql', bodyParser.json(), graphqlExpress({
+      context: {
+        mongo
+      }, // 4
+      schema
+    }));
+    app.use('/graphiql', graphiqlExpress({
+      endpointURL: '/graphql',
+    }));
 
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log(`Hackernews GraphQL server running on port ${PORT}.`)
-});
+    const PORT = 3000;
+    app.listen(PORT, () => {
+      console.log(`Hackernews GraphQL server running on port ${PORT}.`)
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+// 5
+start();
--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,18 +1,18 @@
-const links = [
-    {
-      id: 1,
-      url: 'http://graphql.org/',
-      description: 'The Best Query Language'
+module.exports = {
+  Query: {
+    allLinks: async (root, data, {mongo: {Links}}) => { // 1
+      return await Links.find({}).toArray(); // 2
     },
-    {
-      id: 2,
-      url: 'http://dev.apollodata.com',
-      description: 'Awesome GraphQL Client'
+  },
+
+  Mutation: {
+    createLink: async (root, data, {mongo: {Links}}) => {
+      const response = await Links.insert(data); // 3
+      return Object.assign({id: response.insertedIds[0]}, data); // 4
     },
-  ];
-  
-  module.exports = {
-    Query: {
-      allLinks: () => links,
-    },
-  };
+  },
+
+  Link: {
+    id: root => root._id || root.id, // 5
+  },
+};
